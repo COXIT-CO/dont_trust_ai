8bb63e32a5e1510a5932ad2a8ee23a0841d56b69
eLQeR <yaroslavbiziuk@gmail.com>
2024-08-12 13:01:01 +0300

    Make async version of testing prompt, and dockerize app

---
+++ openrouter.py
@@ -0,0 +1,19 @@
+from config import client
+
+
+async def llm_call(
+        prompt: str, llm_model: str, temperature: float, options: str, instruction: str, input_text: str
+):
+    completion = await client.chat.completions.create(
+        model=llm_model,
+        temperature=temperature,
+        messages=[
+            {
+                "role": "assistant",
+                "content": prompt.format(
+                    OPTIONS=options, INSTRUCTION=instruction, INPUT_TEXT=input_text
+                ),
+            },
+        ],
+    )
+    return completion.choices[0].message.content
