5e773e8f3949d427b79f0bfcf51215fdd7223d18
eLQeR <yaroslavbiziuk@gmail.com>
2024-08-15 14:18:31 +0300

    Add testcases preview in streamlit app

--- src/app.py
+++ src/app.py
@@ -1,18 +1,18 @@
 import asyncio
-
 import streamlit as st
-from config import DEFAULT_LLM_MODEL, DEFAULT_LLM_TEMPERATURE
-from constants import OPTIONS, INSTRUCTION, PROMPT
-from main import get_test_prompt
 
-
-# Function to run the async code within the synchronous Streamlit context
+from config import OPENROUTER_DEFAULT_LLM_MODELS
+from inputs import PROMPTS_CONFIG, TESTCASES_LIST
+from main import test_prompt
 
 
 def async_get_test(*args, **kwargs):
+    """
+    Function to run the async code within the synchronous Streamlit context
+    """
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    return loop.run_until_complete(get_test_prompt(*args, **kwargs))
+    return loop.run_until_complete(test_prompt(*args, **kwargs))
 
 
 # Streamlit app code
@@ -24,37 +24,32 @@ def main():
     st.header("Test your prompt")
 
     with st.form("my_form"):
-        input_llm = st.text_input(
-            "Input LLM model: ", key="input_llm", value=DEFAULT_LLM_MODEL
+        selected_llm = st.selectbox('Choose LLM model: ', OPENROUTER_DEFAULT_LLM_MODELS)
+        selected_prompt = st.selectbox('Choose prompt:', PROMPTS_CONFIG.keys())
+        selected_testcases = st.multiselect(
+            "Choose testcases:",
+            TESTCASES_LIST,
+            TESTCASES_LIST
         )
-        input_temp = st.text_input(
-            "Input temperature of model: ", key="input_temp", value=DEFAULT_LLM_TEMPERATURE
-        )
-
-        input_options = st.text_area("Input your options: ", key="input_options", value=OPTIONS)
-
-        input_instruction = st.text_area(
-            "Input your instruction: ", key="input_instruction", value=INSTRUCTION, height=250
-        )
-
-        input_prompt = st.text_area("Input your prompt: ", key="input_prompt", value=PROMPT, height=350)
-
         submitted = st.form_submit_button("Let's test")
 
         if submitted:
             with st.spinner("Fetching data..."):
-                try:
-                    short_response, long_response = async_get_test(
-                        input_prompt, input_llm, input_temp, input_options, input_instruction
-                    )
-                except Exception as e:
-                    st.error(e)
-                    return
+                short_response, long_response = async_get_test(
+                    prompt_number=selected_prompt,
+                    llm_model=selected_llm,
+                    testcases=selected_testcases
+                )
             st.subheader("Short Result: ")
             st.write(short_response)
             st.subheader("------------------------------------------------------------")
             st.subheader("Long Result: ")
             st.write(long_response)
+
+    st.header("Testcases Preview")
+    for index, specification, expected_result in TESTCASES_LIST:
+        st.text_area(f"Testcase-{index}: ", value=specification, height=350)
+        st.text_area(f"Expected Result-{index}: ", value=expected_result, height=30)
 
 
 if __name__ == "__main__":
