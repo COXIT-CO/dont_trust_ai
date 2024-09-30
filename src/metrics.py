23c83991d81c73b0c09b976952d0f24f9c26f3e7
eLQeR <yaroslavbiziuk@gmail.com>
2024-09-30 11:26:02 +0300

    Remove deepeval.py file. Update metics.py with docstring and get_selected_metrics function

--- src/metrics.py
+++ src/metrics.py
@@ -1,79 +1,33 @@
-from typing import Optional
+from deepeval.metrics import (
+    AnswerRelevancyMetric,
+    FaithfulnessMetric,
+    HallucinationMetric,
+    SummarizationMetric,
+    ToxicityMetric,
+    BiasMetric,
+    ContextualRelevancyMetric,
+    ContextualRecallMetric,
+    ContextualPrecisionMetric,
+    GEval,
+)
 
-import pandas as pd
-from deepeval.dataset import EvaluationDataset
-from deepeval.metrics import GEval
-from deepeval.test_case import LLMTestCaseParams, LLMTestCase
+from deepeval.test_case import LLMTestCaseParams
 
-
-class CustomEvaluationDataset(EvaluationDataset):
-    def add_test_cases_from_dataframe(
-        self,
-        df: pd.DataFrame,
-        input_col_name: str,
-        actual_output_col_name: str,
-        expected_output_col_name: Optional[str] = None,
-        context_col_name: Optional[str] = None,
-        context_col_delimiter: str = ";",
-        retrieval_context_col_name: Optional[str] = None,
-        retrieval_context_col_delimiter: str = ";",
-    ):
-        """
-        Load test cases from a Pandas Dataframe.
-        """
-
-        def get_column_data(df: pd.DataFrame, col_name: str, default=None):
-            return (
-                df[col_name].values if col_name in df.columns else [default] * len(df)
-            )
-
-        inputs = get_column_data(df, input_col_name)
-        actual_outputs = get_column_data(df, actual_output_col_name)
-        expected_outputs = get_column_data(df, expected_output_col_name, default=None)
-        contexts = [
-            context[0].split(context_col_delimiter) if context else []
-            for context in get_column_data(df, context_col_name, default="")
-        ]
-        retrieval_contexts = [
-            (
-                retrieval_context.split(retrieval_context_col_delimiter)
-                if retrieval_context
-                else []
-            )
-            for retrieval_context in get_column_data(
-                df, retrieval_context_col_name, default=""
-            )
-        ]
-
-        for (
-            input,
-            actual_output,
-            expected_output,
-            context,
-            retrieval_context,
-        ) in zip(
-            inputs,
-            actual_outputs,
-            expected_outputs,
-            contexts,
-            retrieval_contexts,
-        ):
-            self.add_test_case(
-                LLMTestCase(
-                    input=input,
-                    actual_output=actual_output,
-                    expected_output=expected_output,
-                    context=context,
-                    retrieval_context=retrieval_context,
-                )
-            )
+# Constants
+DEFAULT_THRESHOLD = 0.5
 
 
 class CorrectnessMetric(GEval):
-    def __init__(self, threshold: float = 0.5):
+    """
+    Metric for evaluating correctness by ensuring that the selected
+    result matches the expected result based on a series of material
+    selection steps.
+    """
+
+    def __init__(self, threshold: float = DEFAULT_THRESHOLD):
         super().__init__(
             name="Correctness",
-            criteria="Ensure that the result series match the expected result series",
+            criteria="Ensure that the result series match the expected result series.",
             evaluation_steps=[
                 "1. Verify that the core material is correctly chosen according to the expected result.",
                 "2. Verify that the material for fronts is selected correctly, according to the expected result.",
@@ -91,15 +45,34 @@ class CorrectnessMetric(GEval):
 
 
 # List of available metrics
-METRICS_LIST = [
-    "Correctness",
-    "Answer Relevancy",
-    "Faithfulness",
-    "Hallucination",
-    "Summarization",
-    "Toxicity",
-    "Bias",
-    "Contextual Recall",
-    "Contextual Relevancy",
-    "Contextual Precision",
-]
+METRICS_DICT = {
+    "Correctness": CorrectnessMetric,
+    "Answer Relevancy": AnswerRelevancyMetric,
+    "Faithfulness": FaithfulnessMetric,
+    "Hallucination": HallucinationMetric,
+    "Summarization": SummarizationMetric,
+    "Toxicity": ToxicityMetric,
+    "Bias": BiasMetric,
+    "Contextual Recall": ContextualRecallMetric,
+    "Contextual Relevancy": ContextualRelevancyMetric,
+    "Contextual Precision": ContextualPrecisionMetric,
+}
+
+
+def get_selected_metrics(selected_metrics: dict):
+    """
+    Retrieve the selected metrics with their associated thresholds.
+
+    Args:
+        selected_metrics (dict): Dictionary of selected metrics with threshold values.
+
+    Returns:
+        list: A list of instantiated metric objects.
+    """
+
+    return [
+        METRICS_DICT[metric](threshold=threshold)
+        for metric, threshold in selected_metrics.items()
+    ]
+
+
